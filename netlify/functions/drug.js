d0d21157f4e713e2148694856c518e3854a5c9e4
wntjsl0923-sys <wntjsl0923@gmail.com>
2026-05-03 15:59:36 +0900

    Add files via upload

---
+++ netlify/functions/drug.js
@@ -0,0 +1,21 @@
+exports.handler = async (event) => {
+  const name = event.queryStringParameters?.name || '';
+  const API_KEY = '1c109377f9b62c2048b8ed97dec57cb1ceabf944ca88e3a9e8dbef952e37e39';
+  const url = `https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=${encodeURIComponent(API_KEY)}&itemName=${encodeURIComponent(name)}&type=json&numOfRows=10&pageNo=1`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: e.message })
+    };
+  }
+};
